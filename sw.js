788ff485a16335f4be8ee5378c681457a4b5ee19
Ghosty-sudo <113943247+Ghosty-sudo@users.noreply.github.com>
2026-09-03 16:05:44 -0400

    Deploy ASHBOUND 0.31 service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ashbound-0.30.0';
-const CORE=['./','./index.html','./loader-030.js','./ab030.01.b64','./ab030.02.b64','./ab030.03.b64','./ab030.04.b64','./ab030.05.b64','./manifest.webmanifest'];
+const CACHE='ashbound-0.31.0';
+const CORE=["./","./index.html","./loader-031.js","./ab031.01.b64","./ab031.02.b64","./ab031.03.b64","./ab031.04.b64","./ab031.05.b64","./ab031.06.b64","./manifest.webmanifest"];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('ashbound-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).origin!==location.origin)return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{if(r.ok){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
